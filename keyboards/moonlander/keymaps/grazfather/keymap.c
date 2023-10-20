ef66392b2954c67810c15ab8b0e20b9019fa92cf
Grazfather <grazfather@gmail.com>
2023-10-19 21:27:52 -0400

    Try hardcore: Disable number row

--- keyboards/moonlander/keymaps/grazfather/keymap.c
+++ keyboards/moonlander/keymaps/grazfather/keymap.c
@@ -30,7 +30,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Base layer
   [0] = LAYOUT_moonlander(
-    KC_GRAVE,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_6,                           KC_5,           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,
+    KC_GRAVE,       XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,                        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        KC_MINUS,
     KC_TAB,         KC_Q,           KC_W,           KC_F,           KC_P,           KC_V,           XXXXXXX,                        XXXXXXX,        KC_J,           KC_L,           KC_U,           KC_Y,           KC_SCLN,        KC_BSLS,
     LT(4,KC_ESCAPE),KC_A,           KC_R,           KC_S,           KC_T,           KC_G,           XXXXXXX,                        XXXXXXX,        KC_M,           KC_N,           KC_E,           KC_I,           KC_O,           KC_QUOTE,
     KC_LSFT,        KC_Z,           KC_X,           KC_C,           KC_D,           KC_B,                                                           KC_K,           KC_H,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RSFT,
