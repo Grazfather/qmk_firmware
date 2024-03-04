60bd038fe3ef39123e88b8e93cd4287065f11be3
Grazfather <grazfather@gmail.com>
2024-03-04 10:42:20 -0500

    Lulu: Add arrow keys to symbol layer

--- keyboards/boardsource/lulu/keymaps/grazfather/keymap.c
+++ keyboards/boardsource/lulu/keymaps/grazfather/keymap.c
@@ -42,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [1] = LAYOUT(
   KC_ESC,         KC_F1,        KC_F2,         KC_F3,         KC_F4,         KC_F5,                                    KC_F6,         KC_F7,         KC_F8,         KC_F9,         KC_F10,        KC_F11,
-  _______,        KC_NO,        KC_NO,         KC_NO,         KC_NO,         KC_NO,                                    KC_NO,         KC_LBRC,       KC_RBRC,       KC_EQL,        KC_NO,         KC_F12,
-  _______,        KC_NO,        KC_NO,         KC_NO,         KC_NO,         KC_NO,                                    KC_NO,         KC_LPRN,       KC_RPRN,       KC_NO,         KC_NO,         KC_NO,
+  _______,        KC_NO,        KC_NO,         KC_UP,         KC_NO,         KC_NO,                                    KC_NO,         KC_LBRC,       KC_RBRC,       KC_EQL,        KC_NO,         KC_F12,
+  _______,        KC_NO,        KC_LEFT,       KC_DOWN,       KC_RIGHT,      KC_NO,                                    KC_NO,         KC_LPRN,       KC_RPRN,       KC_NO,         KC_NO,         KC_NO,
   _______,        KC_NO,        KC_NO,         KC_NO,         KC_NO,         KC_NO,        _______,      _______,      KC_NO,         KC_LCBR,       KC_RCBR,       KC_NO,         KC_NO,         KC_NO,
                                                _______,       _______,       _______,      _______,      _______,      _______,       _______,       _______
 ),
